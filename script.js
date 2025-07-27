eabd41eaf0bbfc6fbbd8c611fe6ec74512f22eb3
kaaz2y5 <kaaz2y5@outlook.com>
2025-07-27 16:03:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert("Thanks for contacting DragonGlass Shineworks! We'll get back to you soon.");
+});
